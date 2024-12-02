dca7189b16c9d530ccf712258225be66c5485d51
oyatomo <82521012+Oya-Tomo@users.noreply.github.com>
2024-12-02 18:34:16 +0900

    update: add tracks buffer

---
+++ src/track.py
@@ -0,0 +1,22 @@
+from aiortc.contrib.media import MediaRelay, MediaPlayer, MediaStreamTrack
+
+
+class Tracks:
+    def __init__(self) -> None:
+        self.relay = MediaRelay()
+        self.players: dict[str, MediaPlayer] = {}
+
+    def create_video_track(
+        self, device: str, framerate: int, video_size: str
+    ) -> MediaStreamTrack:
+        player = self.players.get(device)
+        if player == None:
+            player = MediaPlayer(
+                device,
+                format="v4l2",
+                options={
+                    "framerate": str(framerate),
+                    "video_size": video_size,
+                },
+            )
+        return self.relay.subscribe(player)
